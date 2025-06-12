dc5ded7e51259007806b51b7d0420209f5f73a72
Your Namelesonnhat <sonnhatbest@gmail.com>
2025-06-12 14:28:44 +0700

    Thêm frontend Đăng ký + Đăng nhập

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import "./globals.css";
-import Navbar from "@/components/Navbar";
+import Navbar from "../components/Navbar";
 import React from "react"
 
 export const metadata = {
---
+++ src/components/JoinSection.jsx
@@ -0,0 +1,9 @@
+const JoinSection = () => {
+    return (
+        <div>
+            <h2>Welcome to Quiz</h2>
+        </div>
+    );
+};
+
+export default JoinSection;
