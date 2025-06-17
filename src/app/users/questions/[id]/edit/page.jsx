ff8c5bc13490d7576314f84c646216b4bab7295d
Kingsman1996 <trader96non@gmail.com>
2025-06-18 00:36:27 +0700

    sửa component tạo + sửa nội dung câu hỏi

--- src/app/users/questions/[id]/edit/page.jsx
+++ src/app/users/questions/[id]/edit/page.jsx
@@ -1,63 +1,10 @@
 "use client"
 
-import {useEffect, useState} from "react"
-import {useParams} from "next/navigation"
-import QuestionFormUI from "../../../../../components/CreateOrEditQuestion"
-import QuestionService from "../../../../../services/QuestionService"
-import {Loader2} from "lucide-react"
-import {initialAnswers} from "../../../../../initvalues/answer";
 
-export default function EditQuestion() {
-    const {id} = useParams()
-    const [question, setQuestion] = useState(null)
-    const [loading, setLoading] = useState(true)
+import EditQuestionForm from "../../../../../components/EditQuestionForm";
 
-    function applyColorsToAnswers(answers, type) {
-        const template = initialAnswers(type)
-        return answers.map((a, index) => ({
-            ...a,
-            color: a.color || template[index]?.color || "from-gray-400 to-gray-600"
-        }))
-    }
-
-
-    useEffect(() => {
-        async function fetchQuestion() {
-            try {
-                const res = await QuestionService.getById(id)
-                const question = res.data
-
-                question.answers = applyColorsToAnswers(question.answers, question.type.name)
-                question.type = question.type.name
-                question.category = question.category.name
-                question.difficulty = question.difficulty.name
-
-                setQuestion(question)
-            } catch (error) {
-                console.error("Failed to fetch question:", error)
-            } finally {
-                setLoading(false)
-            }
-        }
-
-        fetchQuestion()
-    }, [id])
-
-
-
-    if (loading || !question) {
-        return (
-            <div className="flex items-center justify-center min-h-screen bg-purple-900">
-                <Loader2 className="h-8 w-8 animate-spin text-white"/>
-            </div>
-        )
-    }
-
+export default function Component() {
     return (
-        <QuestionFormUI
-            initialValues={question}
-            isEdit={true}
-            questionId={id}
-        />
+        <EditQuestionForm/>
     )
 }
--- src/app/users/questions/create/page.jsx
+++ src/app/users/questions/create/page.jsx
@@ -1,24 +1,9 @@
 "use client"
+import CreateQuestionForm from "../../../../components/CreateQuestionForm";
 
-import { useState } from "react"
-import {initialAnswers} from "../../../../initvalues/answer";
-import QuestionFormUI from "../../../../components/CreateOrEditQuestion";
 
-const defaultType = "single"
-
-export default function QuestionCreateForm() {
-    const [initialValues, setInitialValues] = useState({
-        category: "",
-        difficulty: "",
-        type: defaultType,
-        content: "",
-        answers: initialAnswers(defaultType)
-    })
-
+export default function Component() {
     return (
-        <QuestionFormUI
-            initialValues={initialValues}
-            isEdit={false}
-        />
+        <CreateQuestionForm/>
     )
 }
