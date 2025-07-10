0fa953185a5caa97b4fe990fa0faf2df042b4939
npv34 <phanvanluan611996@gmail.com>
2025-07-10 15:38:40 +0700

    su dung lazy loading image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false, // ✅ Tắt Strict Mode
+    images: {
+        domains: [process.env.NEXT_PUBLIC_SUPABASE_DOMAIN],
+    },
 };
 
 module.exports = nextConfig;
